f79533397e6e3136a8ba5f99797308846979a694
Alejandro-Villa <avpcolequinto@gmail.com>
2019-07-17 18:41:47 +0200

    Done: Make it so it read from a file

--- CLI-Greeter.sh
+++ CLI-Greeter.sh
@@ -9,15 +9,22 @@
 
 columns=$(tput cols) # Get total of columns of the terminal window
 
-quotes_array=(
-"Las especies que sobreviven no son las más fuertes, ni las más rápidas, ni las más inteligentes; sino aquellas que se adaptan mejor al cambio.\n-- Charles Darwin"
-"Las grandes almas siempre se han encontrado con una oposición violenta de las mentes mediocres.\n-- Albert Einstein"
-"La religión es el opio del pueblo.\n-- Karl Marx"
-"Si la montaña no viene a ti, ve tú a la montaña.\n-- Mahoma"
-"Nuestra mayor gloria no está en no caer nunca, sino en levantarnos cada vez que caemos.\n-- Confucio"
-"No hay camino para la verdad, la verdad es el camino.\n-- Mahatma Gandhi"
-"Hijo mío, la felicidad está hecha de pequeñas cosas, un pequeño yate, una pequeña mansión, una pequeña fortuna.\n-- Groucho Marx"
-)
+#quotes_array=(
+#"Las especies que sobreviven no son las más fuertes, ni las más rápidas, ni las más inteligentes; sino aquellas que se adaptan mejor al cambio.\a-- Charles Darwin"
+#"Las grandes almas siempre se han encontrado con una oposición violenta de las mentes mediocres.\a-- Albert Einstein"
+#"La religión es el opio del pueblo.\a-- Karl Marx"
+#"Si la montaña no viene a ti, ve tú a la montaña.\a-- Mahoma"
+#"Nuestra mayor gloria no está en no caer nunca, sino en levantarnos cada vez que caemos.\a-- Confucio"
+#"No hay camino para la verdad, la verdad es el camino.\a-- Mahatma Gandhi"
+#"Hijo mío, la felicidad está hecha de pequeñas cosas, un pequeño yate, una pequeña mansión, una pequeña fortuna.\a-- Groucho Marx"
+#)
+
+i=0
+while read -r LINE 
+do
+    quotes_array[$i]=$LINE
+    i=$i+1
+done < $1
 
 # Selects a random quote from the array using the system time as a seed
 RANDOM=$$$(date +%s)
@@ -29,7 +36,7 @@ IFS='\' read -ra printable_array <<< $selected_quote
 # Now the tricky part. Centering and cutting the edges out. For this I have used 
 # printf and a simple for loop with the chosen length of the quote on-screen
 line_size=$(($columns - 10)) # setting line size as columns
-printable_array[1]=${printable_array[1]:1:${#printable_array[1]}} # Removing the 'n' 
+printable_array[1]=${printable_array[1]:1:${#printable_array[1]}} # Removing the 'a' 
 
 if [[ ${#printable_array[0]} -lt $line_size ]]; then
     printf "%*s\n" $(( (${#printable_array[0]} + $columns) / 2 )) "${printable_array[0]}"
