ee0a000d59825e482bbf8ce39e9397782a3fe83b
Alejandro-Villa <avpcolequinto@gmail.com>
2019-07-17 18:43:26 +0200

    Code cleanup and version change

--- CLI-Greeter.sh
+++ CLI-Greeter.sh
@@ -5,19 +5,9 @@
 # The printed string will be centered and cropped out leaving 20 columns on each 
 # side of the terminal.
 #
-# Version 1.0
+# Version 2.0
 
 columns=$(tput cols) # Get total of columns of the terminal window
-
-#quotes_array=(
-#"Las especies que sobreviven no son las más fuertes, ni las más rápidas, ni las más inteligentes; sino aquellas que se adaptan mejor al cambio.\a-- Charles Darwin"
-#"Las grandes almas siempre se han encontrado con una oposición violenta de las mentes mediocres.\a-- Albert Einstein"
-#"La religión es el opio del pueblo.\a-- Karl Marx"
-#"Si la montaña no viene a ti, ve tú a la montaña.\a-- Mahoma"
-#"Nuestra mayor gloria no está en no caer nunca, sino en levantarnos cada vez que caemos.\a-- Confucio"
-#"No hay camino para la verdad, la verdad es el camino.\a-- Mahatma Gandhi"
-#"Hijo mío, la felicidad está hecha de pequeñas cosas, un pequeño yate, una pequeña mansión, una pequeña fortuna.\a-- Groucho Marx"
-#)
 
 i=0
 while read -r LINE 
@@ -35,9 +25,12 @@ IFS='\' read -ra printable_array <<< $selected_quote
 
 # Now the tricky part. Centering and cutting the edges out. For this I have used 
 # printf and a simple for loop with the chosen length of the quote on-screen
+
 line_size=$(($columns - 10)) # setting line size as columns
 printable_array[1]=${printable_array[1]:1:${#printable_array[1]}} # Removing the 'a' 
 
+
+# Printing the quote
 if [[ ${#printable_array[0]} -lt $line_size ]]; then
     printf "%*s\n" $(( (${#printable_array[0]} + $columns) / 2 )) "${printable_array[0]}"
     printf "%*s\n" $(( (${#printable_array[1]} + $columns) / 2 )) "${printable_array[1]}"
